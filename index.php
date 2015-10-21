18993e3842b54d3cb81fa660cc01ed42e181e513
mohamed amine <MohamedAmine1C@gmail.com>
2015-10-21 17:50:46 +0100

    Fix Single File setName method

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'autoload.php';
 
 if (isset($_POST['submit'])) {
-  $file = new upload\MultiFile('myfile');
+  $file = new upload\File('myfile');
   $file->setName("amine");
   $file->size(1000000)->Extension('jpg,png,jpeg')->Exist();
   $file->Upload($_SERVER["DOCUMENT_ROOT"]) or var_dump($file->errors);
@@ -10,8 +10,7 @@ if (isset($_POST['submit'])) {
 
  ?>
 <form action="#" method="post" enctype="multipart/form-data">
-  <input type="file" name="myfile[]"><br>
-  <input type="file" name="myfile[]"><br>
-  <input type="file" name="myfile[]"><br>
+  <input type="file" name="myfile"><br>
+
   <input type="submit" name="submit" value="submit">
 </form>
--- upload/File.php
+++ upload/File.php
@@ -8,7 +8,7 @@ class File extends Validate
 
   function __construct($key, $File = false)
   {
-    if (!isset($File)) {
+    if (!$File) {
       $File = array(
         'name' => $_FILES[$key]['name'],
         'tmp_name' => $_FILES[$key]['tmp_name']
@@ -18,10 +18,11 @@ class File extends Validate
     $FileName = explode(".", $File["name"]);
     $this->setName($FileName[0]);
     $this->setExtension(strtolower(end(explode(".", $File["name"]))));
+    
     // $File Will be passed to $FilesToUpload so Upload() be able to upload multipal Files
     $File['Extension'] = $this->getExtension();
     $this->FilesToUpload[] = $File;
-
+    
     parent::__construct($FilePath);
   }
 
@@ -31,7 +32,8 @@ class File extends Validate
       if (empty($this->errors)) {
         foreach ($this->FilesToUpload as $File) {
           if ($File['name']) {
-            $Path = $newPath.DIRECTORY_SEPARATOR.$File['name'];
+            $Path = $this->FilesToUpload[1]['name'] ? $newPath.DIRECTORY_SEPARATOR.$this->getName($File) : $newPath.DIRECTORY_SEPARATOR.$this->getName().'.'.$this->getExtension() ;
+            var_dump($Path);
             $uploadedFiles[] = array('name' => $File['name'], 'dir' => $Path);
             move_uploaded_file($File['tmp_name'], $Path);
           }
